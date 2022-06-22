c3b6ac57132a703de088c8ac319f74b8969829b4
VijayaprakashVp <prakashvijay905@gmail.com>
2022-06-22 22:11:58 +0530

    Initial Commit. Packages installed

---
+++ weather_app/src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Weather } from "./Components/Weather";
+import { Heading } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div className="App">
+      <Heading>Weather App</Heading>
+      <Routes>
+        <Route path="/" element={<Weather />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
